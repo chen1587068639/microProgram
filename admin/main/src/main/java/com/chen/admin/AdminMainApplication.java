43103ba1b498db8e12c2a238e32c5416c9c95295
chengw <1587068639@qq.com>
2022-09-07 15:59:40 +0800

    admin项目初始化

---
+++ admin/main/src/main/java/com/chen/admin/AdminMainApplication.java
@@ -0,0 +1,13 @@
+package com.chen.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminMainApplication.class, args);
+    }
+
+}
